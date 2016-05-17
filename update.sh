a56693a7016b6ddb9980002ce7b2c201c90aca42
Faraz Fazli <farazfazli@gmail.com>
2016-05-16 23:54:48 -0400

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+# Version grabbing taken from install.sh script in Apex https://github.com/apex/apex
+LATEST=$(curl -s https://api.github.com/repos/coreos/rkt/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+URL="https://github.com/coreos/rkt/releases/download/$LATEST/rkt-$LATEST.tar.gz"
+if [ ! -d rkt-$LATEST ]; then
+        echo Updating to rkt-$LATEST
+        wget $URL && tar -xzvf rkt-$(LATEST).tar.gz && rm rkt-$LATEST.tar.gz
+else
+        echo Already on latest version: $LATEST
+fi
+
+LATEST=$(curl -s https://api.github.com/repos/appc/acbuild/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
+URL="https://github.com/appc/acbuild/releases/download/$LATEST/acbuild.tar.gz"
+
+if [ $(cat acbuild.txt | grep $LATEST | wc -l | xargs) -lt 1 ]; then
+        echo "Updating to acbuild $LATEST"
+        wget $URL && tar -xzvf acbuild && rm acbuild.tar.gz
+        echo $LATEST > acbuild.txt
+else
+        echo "Already on latest acbuild version: $LATEST"
+fi
